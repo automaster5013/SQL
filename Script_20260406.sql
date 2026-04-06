1fd761bde9df7648d74c4b17ab305cfa4dcde054
myeonghwi <automaster5013@gmail.com>
2026-04-06 15:43:42 +0900

    Oracle_SQL_Table_생성하기

--- Script_20260406.sql
+++ Script_20260406.sql
@@ -16,3 +16,16 @@ create table department (
 		office 		varchar2(20),
 		constraint pk_department primary key(dept_id)
 	)
+	
+CREATE TABLE professor (
+	prof_id		varchar2(10),
+	resident_id		varchar2(14)	NOT null,
+	name		varchar2(10)		NOT null,
+	dept_id		varchar2(10),
+	position		varchar2(10),
+	year_emp	int,
+		constraint 	pk_professor 	primary key(prof_id),
+		constraint 	fk_professor 	foreign key(dept_id) 
+					references department(dept_id)
+	)
+	
